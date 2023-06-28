bf4991ca2fd6d878ff76241e1461ea758728c628
MiaAntunes <suportecamilaantunes2003@hotmail.com>
2023-06-28 10:39:45 -0300

    DOCS : implementação de uma lógica de estudo

--- build/index.js
+++ build/index.js
@@ -1,3 +1,7 @@
 "use strict";
-console.log("Deu certo");
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+const database_2 = require("./database");
+console.log("Todos os usuários", database_1.dataBaseUser);
+console.log("Todos os produtos", database_2.dataBaseProduct);
 //# sourceMappingURL=index.js.map
